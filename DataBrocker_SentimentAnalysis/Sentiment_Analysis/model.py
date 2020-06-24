262e1045ec9d1ed1e21e578e557da2fecd718da8
saj04481 <sajid.naeem@iais.fraunhofer.de>
2020-06-24 19:48:35 +0200

    Sentiment Analysis Pipeline

---
+++ DataBrocker_SentimentAnalysis/Sentiment_Analysis/model.py
@@ -0,0 +1,29 @@
+from keras.preprocessing.text import one_hot,Tokenizer
+from keras.models import load_model
+import warnings
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+
+
+
+
+def classify_review(review):
+	maxlen = 100
+	model = load_model('model.h5')
+	d = imdb.get_word_index()
+	words = review.split()
+	review = []
+	for word in words:
+		if word not in d:
+			review.append(2)
+		else:
+			review.append(d[word] + 3)
+
+	review = sequence.pad_sequences([review],
+									truncating='pre', padding='pre', maxlen=maxlen)
+	prediction = model.predict(review)
+	return prediction[0][0]
+
+
+
