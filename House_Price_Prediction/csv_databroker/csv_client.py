f355a9bd0746c663ec3f9f3229ede383c3cc6590
mwelss-fhg <martin.welss@iais.fraunhofer.de>
2021-03-30 21:33:29 +0200

    add house price csv databroker

---
+++ House_Price_Prediction/csv_databroker/csv_client.py
@@ -0,0 +1,29 @@
+import grpc
+from random import randint
+from timeit import default_timer as timer
+
+# import the generated classes
+import model_pb2
+import model_pb2_grpc
+from google.protobuf import empty_pb2
+
+start_ch = timer()
+port_addr = 'localhost:50080'
+# open a gRPC channel
+channel = grpc.insecure_channel(port_addr)
+# create a stub (client)
+stub = model_pb2_grpc.get_next_rowStub(channel)
+end_ch = timer()
+
+request = empty_pb2.Empty()
+nextrow = stub.get_next_row(request)
+
+print('The next row is :', nextrow)
+print('status of response:', grpc.StatusCode.OK.value)
+#print('status of response:', grpc.StatusCode.name)
+for i in grpc.StatusCode:
+    print(i, i.value)
+
+
+
+
---
+++ House_Price_Prediction/csv_databroker/csv_server.py
@@ -0,0 +1,55 @@
+import grpc
+from concurrent import futures
+import time
+
+# import the generated classes :
+import model_pb2
+import model_pb2_grpc
+
+import get_next_row as gnr
+
+port = 50080
+row_obj = gnr.FetchRowCSV()
+current_row = 0
+
+class get_next_rowServicer(model_pb2_grpc.get_next_rowServicer):
+    #row_obj = gnr.FetchRowCSV()
+    def get_next_row(self, request, context):
+        response = model_pb2.Features()
+        total_rows = row_obj.init_count()
+        current_row = row_obj.current_row
+        print("total number of rows of csv: ", total_rows)
+        if current_row % total_rows == 0 and current_row != 0:
+            response.MSSubClass = float(0)
+            response.LotArea = float(0)
+            response.YearBuilt = float(0)
+            response.BedroomAbvGr = float(0)
+            response.TotRmsAbvGrd = float(0)
+        else:
+            print("entered else:")
+            row = row_obj.get_next_row(current_row)
+            # row = gnr.get_next_row(request.count)
+            # row = gnr.get_next_row()
+            response.MSSubClass = row[0]
+            response.LotArea = row[1]
+            response.YearBuilt = row[2]
+            response.BedroomAbvGr = row[3]
+            response.TotRmsAbvGrd = row[4]
+            row_obj.current_row = row_obj.current_row + 1
+
+
+        return response
+
+# creat a grpc server :
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+model_pb2_grpc.add_get_next_rowServicer_to_server(get_next_rowServicer(), server)
+print("Starting Server. Listening to port :" + str(port))
+server.add_insecure_port("[::]:{}".format(port))
+server.start()
+
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
