f011c5fe03ba17600258a5de27f9c2e62371641f
Han Tran <htran@know-center.at>
2021-09-28 13:34:27 +0200

    add: license

--- openml_data_integration/protobuf_generator/main.py
+++ openml_data_integration/protobuf_generator/main.py
@@ -10,8 +10,8 @@ def main(argv):
         idx = int(argv[0])
     except:
         # If not provided from Terminal, use some default
-        #idx = 42563
-        idx = 61
+        idx = 42563
+        # idx = 61
 
     # get file_name for output
     try:
@@ -27,7 +27,8 @@ def main(argv):
         # change output folder
         output_folder = ''
 
-    write_proto(dataID=idx, file_name=file_name, output_folder=output_folder)
+    write_proto(dataID=idx, file_name=file_name, output_folder=output_folder,
+                                        license_filename="license-1.0.0.json")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
