df67ea80edd408a656ab4ee52cc2c57d18821a86
Han Tran <htran@know-center.at>
2021-11-04 08:49:56 +0100

    mod: host

--- openml_data_integration/protobuf_generator/docker_build_push.sh
+++
@@ -1,5 +0,0 @@
-
-cd ./openml_$2
-docker build -t openml/$1_$2:v1 .
-docker tag openml/$1_$2:v1 cicd.ai4eu-dev.eu:7444/ai4eu-experiments/openml/$1_$2:v1
-docker push cicd.ai4eu-dev.eu:7444/ai4eu-experiments/openml/$1_$2:v1
